ad10b9f1bfed22811f315ef795fbcb0e9778c36c
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-23 21:41:14 +0100

    Seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create!(text: "Asalaam alaikum")
+Message.create!(text: "Hello!")
+Message.create!(text: "Hola!")
+Message.create!(text: "Bonjour")
+Message.create!(text: "Guten tag")
+Message.create!(text: "konnichiwa")
+Message.create!(text: "Namaste")
+Message.create!(text: "Merhaba")
